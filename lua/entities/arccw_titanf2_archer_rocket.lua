95f974b8daf12eb2dc3ba80a044d6155f1c84ac6
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-11-22 16:42:25 -0500

    select and kill icon wip

--- lua/entities/arccw_titanf2_archer_rocket.lua
+++ lua/entities/arccw_titanf2_archer_rocket.lua
@@ -24,6 +24,6 @@ ENT.DragCoefficient = 0.1
 
 ENT.Force = 12000 -- for LVS support. fuckhuge because this thing is meant to take down titans
 
--- if CLIENT then
-    -- killicon.Add( "arccw_waw_rocket_panzer", "arccw/weaponicons/arccw_waw_panzerschreck", Color( 255, 255, 255, 255 ) )
--- end
+if CLIENT then
+    killicon.Add("arccw_titanf2_archer_rocket", "vgui/killicon_archer_at", Color(251, 85, 25, 255))
+end
