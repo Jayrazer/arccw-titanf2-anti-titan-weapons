a6b817217d2d2576dc9a6e5b77c8fba3420d851d
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-11-25 21:56:30 -0500

    Update arccw_titanf2_mgl_nade.lua

--- lua/entities/arccw_titanf2_mgl_nade.lua
+++ lua/entities/arccw_titanf2_mgl_nade.lua
@@ -15,7 +15,9 @@ ENT.ImpactFuse = true // projectile explodes on impact.
 ENT.ExplodeOnDamage = false // projectile explodes when it takes damage.
 ENT.ExplodeUnderwater = true
 
-ENT.Delay = 2
+ENT.SmokeTrail = true
+
+ENT.Delay = 1.25
 
 function ENT:Detonate(ent)
     local attacker = self.Attacker or self:GetOwner() or self
@@ -37,4 +39,38 @@ function ENT:Detonate(ent)
     self:EmitSound("weapons/mgl/explo_mgl_close_2ch_v1_01.wav", 125, 100, 1, CHAN_AUTO)
 
     self:Remove()
+end
+
+local smokeimages = {"particle/smokesprites_0001", "particle/smokesprites_0002", "particle/smokesprites_0003", "particle/smokesprites_0004", "particle/smokesprites_0005", "particle/smokesprites_0006", "particle/smokesprites_0007", "particle/smokesprites_0008", "particle/smokesprites_0009", "particle/smokesprites_0010", "particle/smokesprites_0011", "particle/smokesprites_0012", "particle/smokesprites_0013", "particle/smokesprites_0014", "particle/smokesprites_0015", "particle/smokesprites_0016"}
+local function GetSmokeImage()
+    return smokeimages[math.random(#smokeimages)]
+end
+
+function ENT:DoSmokeTrail()
+    if CLIENT and self.SmokeTrail then
+        local emitter = ParticleEmitter(self:GetPos())
+
+        local smoke = emitter:Add(GetSmokeImage(), self:GetPos())
+
+        smoke:SetStartAlpha(50)
+        smoke:SetEndAlpha(0)
+
+        smoke:SetStartSize(2)
+        smoke:SetEndSize(math.Rand(10, 15))
+
+        smoke:SetRoll(math.Rand(-180, 180))
+        smoke:SetRollDelta(math.Rand(-1, 1))
+
+        smoke:SetPos(self:GetPos())
+        smoke:SetVelocity(-self:GetAngles():Forward() * 400 + (VectorRand() * 10))
+
+        smoke:SetColor(200, 200, 200)
+        smoke:SetLighting(true)
+
+        smoke:SetDieTime(math.Rand(0.15, 0.25))
+
+        smoke:SetGravity(Vector(0, 0, 0))
+
+        emitter:Finish()
+    end
 end
